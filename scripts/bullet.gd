951f940c9a1001d0d8e1b96a125664ef19153b2e
TJ Klint <timothyjklint@gmail.com>
2024-09-17 02:00:18 -0400

    feat: bullet now flips and shoots left when player looks left.

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -4,10 +4,8 @@ var bullet_velocity = Vector2(1, 0)
 var speed = 300
 
 func _physics_process(delta: float) -> void:
-	# Move the bullet
-	var collision_info = move_and_collide(bullet_velocity.normalized() * speed * delta)
-	
-	# If there's a collision, handle it
+	# Move the bullet and check for collision
+	var collision_info = move_and_collide(bullet_velocity.normalized() * delta * speed)
 	if collision_info:
-		# Handle collision here (e.g., destroy the bullet, apply damage, etc.)
-		queue_free()  # Destroy the bullet on collision
+		# If bullet collides with something, queue_free the bullet
+		queue_free()
