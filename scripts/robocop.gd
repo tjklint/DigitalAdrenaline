120aadfbf10cd7f89e92bbde275fae2783de724a
TJ Klint <timothyjklint@gmail.com>
2024-11-30 12:56:44 -0500

    feat: robocop no longer floats and patrols platforms.

--- scripts/robocop.gd
+++ scripts/robocop.gd
@@ -6,18 +6,16 @@ const ENEMY_SPEED = 60
 
 @export var bullet_color: Color = Color(1, 0, 0)  # Red color for enemy bullets
 @onready var gun_marker: Node2D = $GunMarker  # Position to spawn bullets from
-
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(delta: float) -> void:
-	if ray_cast_right.is_colliding():
-		direction = -1
-		animated_sprite.flip_h = true
-		
-	if ray_cast_right.is_colliding():
+	if direction == -1 and not ray_cast_left.is_colliding(): 
 		direction = 1
+		animated_sprite.flip_h = true
+	elif direction == 1 and not ray_cast_right.is_colliding():  
+		direction = -1
 		animated_sprite.flip_h = false
 		
 	position.x += direction * ENEMY_SPEED * delta
